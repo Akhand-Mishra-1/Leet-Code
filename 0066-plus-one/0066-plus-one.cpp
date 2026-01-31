10740608cf1334b23532bde1601839bc37475b95
Akhand pratap mishra <akhandmishra212@gmail.com>
2026-01-31 09:04:42 +0530

    Time: 3 ms (5.54%), Space: 11.5 MB (52.02%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+  vector<int> plusOne(vector<int>& digits)
+{
+    int n = digits.size();
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (digits[i] == 9)
+        {
+            digits[i] = 0;
+        }
+        else
+        {
+            digits[i] += 1;
+            return digits;
+        }
+    }
+
+    // agar sab 9 the (edge case)
+    digits.insert(digits.begin(), 1);
+    return digits;
+}
+
+};
